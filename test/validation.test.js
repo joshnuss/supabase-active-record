c3514344616afc71539c2ab90fe97d2d314d3346
Josh Nussbaum <joshnuss@gmail.com>
2021-02-09 04:10:06 -0500

    Adds tests for custom validations

--- test/validation.test.js
+++ test/validation.test.js
@@ -1,29 +1,14 @@
 import { ActiveRecord, is } from '../'
 
-class Product extends ActiveRecord {
-  static config = {
-    fields: {
-      name: 'string'
-    }
-  }
-}
-
-class Person extends ActiveRecord {
-  static config = {
-    fields: {
-      firstName: 'string',
-      lastName: 'string',
-      occupation: 'string'
-    },
-    validate: {
-      firstName: is.required(),
-      lastName: is.required({allowNull: true}),
-      occupation: is.required({allowBlank: true})
-    }
-  }
-}
-
 test('valid', async () => {
+  class Product extends ActiveRecord {
+    static config = {
+      fields: {
+        name: 'string'
+      }
+    }
+  }
+
   const product = new Product({firstName: "Josh"})
   const result = await product.validate()
 
@@ -34,6 +19,21 @@ test('valid', async () => {
 })
 
 describe('validates required', () => {
+  class Person extends ActiveRecord {
+    static config = {
+      fields: {
+        firstName: 'string',
+        lastName: 'string',
+        occupation: 'string'
+      },
+      validate: {
+        firstName: is.required(),
+        lastName: is.required({allowNull: true}),
+        occupation: is.required({allowBlank: true})
+      }
+    }
+  }
+
   test('null fails validation', async () => {
     const person = new Person()
     const result = await person.validate()
@@ -64,6 +64,46 @@ describe('validates required', () => {
     expect(result).toEqual({
       valid: true,
       errors: {}
+    })
+  })
+})
+
+describe('custom validation', () => {
+  class Person extends ActiveRecord {
+    static config = {
+      fields: {
+        favoriteColor: 'string'
+      },
+      validate: {
+        favoriteColor: (object, key) => {
+          if (object[key] == 'red')
+            return
+
+          return 'must be red'
+        }
+      }
+    }
+  }
+
+  test('valid when return no error', async () => {
+    const person = new Person({favoriteColor: 'red'})
+    const result = await person.validate()
+
+    expect(result).toEqual({
+      valid: true,
+      errors: {}
+    })
+  })
+
+  test('invalid when function returns error', async () => {
+    const person = new Person({favoriteColor: 'blue'})
+    const result = await person.validate()
+
+    expect(result).toEqual({
+      valid: false,
+      errors: {
+        favoriteColor: ['must be red']
+      }
     })
   })
 })
